378843a4fa1a72775ce3f5e70995031108024082
Anubchik <mudzanvsmadara@gmail.com>
2022-05-20 14:26:19 +0500

    first commit

---
+++ LifeScript.js
@@ -0,0 +1,18 @@
+//
+// const name = 'Javohir lox pashsha'
+// console.log(name)
+// // alert(name)
+//
+// function exe(randomSource) {
+//     Math.random(min, max)
+// }
+//
+// const min = 10
+// const max = 20
+// console.log(exe)
+//
+// const meme = function () {
+//     Math.random(10, 20)
+// }
+//
+// console.log(meme)
